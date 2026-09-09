dc8dbc6c76de2ec764c43f42e97739e02c801b6d
hadassa <h0583224959@gmail.com>
2026-09-09 21:07:46 +0300

    הגדרת נתיבי רישום והתחברות עבור משתמשים

--- server/routes/userRoutes.js
+++ server/routes/userRoutes.js
@@ -1,4 +1,11 @@
 const express = require('express');
 const router = express.Router();
+const userController = require('../controllers/userController');
+
+// נתיב רישום משתמש חדש
+router.post('/register', userController.register);
+
+// נתיב התחברות משתמש קיים
+router.post('/login', userController.login);
 
 module.exports = router;
