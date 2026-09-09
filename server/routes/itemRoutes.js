b38c13ea9cbf33fa5349d3b158aacd34b5b6318b
hadassa <h0583224959@gmail.com>
2026-09-09 22:55:47 +0300

    שילוב מידלוור להעלאת קובץ בנתיב יצירת פריט

--- server/routes/itemRoutes.js
+++ server/routes/itemRoutes.js
@@ -1,9 +1,12 @@
 const express = require('express');
 const router = express.Router();
 const { getItems, getItemById, createItem } = require('../controllers/itemController');
+const upload = require('../middlewares/uploadMiddleware');
 
 // נתיב לקבלת כל הפריטים
 router.get('/', getItems);
+
+// נתיב לקבלת פריט בודד לפי מזהה
 router.get('/:id', getItemById);
-router.post('/', createItem);
+router.post('/', upload.single('audio'), createItem);
 module.exports = router;
