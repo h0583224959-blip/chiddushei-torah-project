35e6f1ba2f88d80c91c97705a323167ecd875de7
hadassa <h0583224959@gmail.com>
2026-09-22 16:46:25 +0300

    הוספת כותרת הוראה שיהי אפשר להוריד את הקובץ ישירות לתקיית הוהרדות

--- server/server.js
+++ server/server.js
@@ -15,7 +15,11 @@ const app = express();
 app.use(cors());
 app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
-app.use('/uploads', express.static(path.join(__dirname, 'uploads')));
+app.use('/uploads', express.static(path.join(__dirname, 'uploads'), {
+  setHeaders: (res) => {
+    res.setHeader('Content-Disposition', 'attachment');
+  }
+}));
 
 // ניתובי כתובות
 app.use('/api/users', userRoutes);
